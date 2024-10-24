f269ea912fd99e43b5b7d832cbe30ea8ad6ec96a
Rajitha Mohankumar <mrmrgov@gmail.com>
2024-10-23 22:35:01 -0700

    Added comments

--- pytestAssignmentGoogleFinance/conftest.py
+++ pytestAssignmentGoogleFinance/conftest.py
@@ -13,7 +13,7 @@ def setUp(request):
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     # Initialize the Chrome driver
-    driver = webdriver.Chrome()  # Add the path to ChromeDriver if necessary)
+    driver = webdriver.Chrome(service=Service(), options=chrome_options)  # Add the path to ChromeDriver if necessary)
     driver.get("https://www.google.com/finance/")
     driver.maximize_window()
     request.cls.driver=driver
