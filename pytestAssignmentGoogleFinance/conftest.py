52f2b53e5d5c01114755dbafee2158ba8466af83
Rajitha Mohankumar <mrmrgov@gmail.com>
2024-10-23 18:11:15 -0700

    Added comments

--- pytestAssignmentGoogleFinance/conftest.py
+++ pytestAssignmentGoogleFinance/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from selenium import webdriver
+#Opens a webpage www.google.com/finance on a chrome browser
 
 @pytest.fixture(scope="class")
 def setUp(request):
@@ -7,5 +8,6 @@ def setUp(request):
     driver.get("https://www.google.com/finance/")
     driver.maximize_window()
     request.cls.driver=driver
+# closes the webpage after all tests are run
     yield
     driver.close()
--- pytestAssignmentGoogleFinance/test_stockComparison.py
+++ pytestAssignmentGoogleFinance/test_stockComparison.py
@@ -11,6 +11,7 @@ class TestTwo(BaseClass):
     def test_symbolComparison(self):
         time.sleep(10)
         section = self.driver.find_element(By.XPATH,"//div[contains(text(),'You may be interested in')]/following-sibling::div")
+#4.Compare the stock symbols retrieved from (3) with given test data
 
         stock_symbols=[]
         symbols = section.find_elements(By.TAG_NAME,"a")
@@ -18,11 +19,14 @@ class TestTwo(BaseClass):
             stock_symbols.append(symbol.text)
         print(stock_symbols)
         test_data = ["NFLX", "MSFT", "TSLA"]
+#5. Print all stock symbols that are in (3) but not in given test data
+
         symbols_not_in_testdata =[]
         for symbol in stock_symbols:
             if symbol not in test_data:
                 symbols_not_in_testdata.append(symbol)
         print(f"These are the symbols in {stock_symbols} but not in {test_data}")
+#6. Print all stock symbols that are in given test data but not in (3)
         symbols_in_testdata = []
         for symbol in test_data:
             if symbol not in stock_symbols:
