e9d3d0b43a04698387234947d244e666f86eba19
Rajitha Mohankumar <mrmrgov@gmail.com>
2024-10-23 22:21:02 -0700

    Added comments

--- pytestAssignmentGoogleFinance/conftest.py
+++ pytestAssignmentGoogleFinance/conftest.py
@@ -1,10 +1,21 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+
+
 #1.Opens a webpage www.google.com/finance on a chrome browser
 
 @pytest.fixture(scope="class")
 def setUp(request):
-    driver = webdriver.Chrome()
+    # Configure Chrome options
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")  # Uncomment this line if you want to run tests in headless mode
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+
+    # Initialize the Chrome driver
+    driver = webdriver.Chrome(service=Service(), options=chrome_options)  # Add the path to ChromeDriver if necessary)
     driver.get("https://www.google.com/finance/")
     driver.maximize_window()
     request.cls.driver=driver
