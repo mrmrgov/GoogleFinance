9466f9d7f36799e2cc1f172204c0da07e6f306eb
Rajitha Mohankumar <mrmrgov@gmail.com>
2024-10-23 23:27:36 -0700

    Added comments

--- pytestAssignmentGoogleFinance/conftest.py
+++ pytestAssignmentGoogleFinance/conftest.py
@@ -2,18 +2,23 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
 
 
 #1.Opens a webpage www.google.com/finance on a chrome browser
 
 @pytest.fixture(scope="class")
 def setUp(request):
-    chrome_options = Options()
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = "/c/Program Files/Google/Chrome/Application"  # Replace with your Chrome path
+    #chrome_options = Options()
     chrome_options.add_argument("--headless")  # Uncomment if needed
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     # Initialize the Chrome driver
-    driver = webdriver.Chrome(service=Service(), options=chrome_options)  # Add the path to ChromeDriver if necessary)
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+    #driver = webdriver.Chrome(service=Service(), options=chrome_options)  # Add the path to ChromeDriver if necessary)
     driver.get("https://www.google.com/finance/")
     driver.maximize_window()
     request.cls.driver=driver
